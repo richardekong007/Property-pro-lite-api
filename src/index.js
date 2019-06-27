e36847653d60e681364961af6d71ea3f63a5d838
richardekong007 <ekong.richard@yahoo.com>
2019-06-27 11:03:22 +0100

    Revert "chore(API versioning) version API using URL versioning"

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 
 import express from "express";
-import bodyParser from "body-parser";
-import appV1 from "./route/api.v1.js";
 
 const app = express();
-app.set('space', 4);
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-app.use("/api/v1",appV1);
-app.use("/", appV1);
 app.set('port', 3999);
+app.set('space', 4);
 
 if (process.env.NODE_ENV !== 'test'){
     app.listen (app.get('port'), () => console.log(`Listening on port - ${app.get('port')}`));
