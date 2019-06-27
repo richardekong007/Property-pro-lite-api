7703928f0e97dd2d38b33d1ca97752d851284eb0
Richard <ekong.richard@yahoo.com>
2019-06-27 12:39:14 +0100

    chore(version api) version API through URL versioning add appV1.js to contain future routes implement appV1 as the first version (v1) of the API
    [start #166943424]

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 
 import express from "express";
+import bodyParser from "body-parser";
+import appV1 from "./route/api.v1.js";
 
 const app = express();
 app.set('port', 3999);
 app.set('space', 4);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use("/api/v1", appV1);
+app.use("/",appV1);
+
 
 if (process.env.NODE_ENV !== 'test'){
     app.listen (app.get('port'), () => console.log(`Listening on port - ${app.get('port')}`));
