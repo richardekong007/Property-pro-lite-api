aaec1697133b6d82d97fae82bfc99f772dee1219
Richard <ekong.richard@yahoo.com>
2019-06-28 03:24:42 +0100

    ft(create user) add method to create user modify api.v1.js by including createUser( ) method to create user from request body
    [start #166874584]

--- src/route/api.v1.js
+++ src/route/api.v1.js
@@ -1,12 +1,26 @@
 import Router from "express";
 import StoreManager from "../store/storeManager.js";
+import User from "../entity/user.js";
 import users from "../store/users.js";
 
 const appV1 = Router();
 const userStore = StoreManager.mount(users);
 
+const createUser = (requestBody) =>{
+    const user = new User.Builder()
+        .build();
+    requestBody.id = "";
+    Object.keys(user).forEach(key=>{
+        if (Object.keys(requestBody).includes(key)){
+            user[key] = requestBody[key];
+        }
+    });
+    return user;
+};
+
 appV1.post("/auth/signup", (req,res)=>{
-    userStore.insert(req.body)
+    const user = createUser(req.body);
+    userStore.insert(user)
         .then(result => {
             res.status(201).json({
                 status: "success",
