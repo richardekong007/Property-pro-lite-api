5b262d9d4ecf1c06c5c203acfea688374a720aec
Richard <ekong.richard@yahoo.com>
2019-06-30 01:58:23 +0100

    ft(test endpoint) refactor test specification for route: POST/auth/signin remove all unused import statements refactor test specification for POST/auth/signin route remove all comments below the major test suite
    [start #166875452]

--- test/index.user.specs.js
+++ test/index.user.specs.js
@@ -1,11 +1,8 @@
 import app from "../src/index.js";
 import {describe, beforeEach, it} from "mocha";
 import chai from "chai";
-import supertest from "supertest";
 import User from "../src/entity/user.js";
-import users from "../src/store/users.js"
 import {userStore} from "../src/route/api.v1.js";
-import StoreManager from "../src/store/storeManager.js";
 
 const createUser = () =>{
     const user = new User.Builder()
@@ -57,8 +54,8 @@ describe("api.v1 Route: user", () =>{
 
     describe("POST/auth/signin", () =>{
         it("Should signin an existing user", () =>{
-            const email = "bong2@mail.net";
-            const password = "!@&*#@";
+            const email = user.email;
+            const password = user.password;
             return chai.request(app)
                 .post("/auth/signin")
                 .send({
@@ -75,31 +72,4 @@ describe("api.v1 Route: user", () =>{
     });
 });
 
-// describe("POST/auth/signup", ()=>{
-//     it("should signup a new user", done =>{
-//         Object.keys(aUser).forEach(key => expect(aUser[key]).to.not.be.undefined);
-//         request.post("/auth/signup")
-//             .send(aUser)
-//             .expect(201)
-//             .end((err,res)=>{
-//                     expect(res.body).to.include.keys(resBodyKeys);
-//                     expect(res.body.data).to.include.keys(userResDataKeys);
-//                     done(err);        
-//             });
-//     });
-// });
-
-// describe("POST/auth/signin", ()=>{
-//     it("should signin user", done =>{
-        
-//         request.post("/auth/signin")
-//             .send({email:aUser.email, password:aUser.password})
-//             .expect(200)
-//             .end((err,res) =>{
-//                 expect(res.body).to.include.keys(resBodyKeys);
-//                 expect(res.body.data).to.include.keys(userResDataKeys);
-//                 done(err);
-//             });
-//     });
-// });
 
