5f34b35acf6bf7c08f14206df384581284b31287
Richard <ekong.richard@yahoo.com>
2019-07-18 20:58:34 +0100

    ft(Enable CORS) modify index.js by enabling Cross Origin Resource Sharing
    [finishes #167383750]

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import express from "express";
+import cors from "cors";
 import router from "./route/route.js";
 
 const app = express();
 
 app.set('port', process.env.PORT || 3999);
-app.set('space', 4);
+app.set('json spaces', 4);
+app.use(cors({
+    origin:["http://localhost:4000"],
+    methods:["GET","POST","PATCH","DELETE"],
+    allowedHeaders:["Content-Type","Authorization"]
+}));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use("/api/v1", router);
